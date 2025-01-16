e7dd4f452c0f616bd28cc2d29cd3e4e9425b441c
Anthony Obiako <anthonyobiakoc@gmail.com>
2025-01-16 12:58:00 +0100

    fixed the border color

--- src/app/pages/projects.tsx
+++ src/app/pages/projects.tsx
@@ -7,7 +7,7 @@ const Projects = () => {
   return (
     <div
       id="projects"
-      className="w-full h-full min-h-[400px] sm:min-h-[600px] font-light border border-neutral-300 dark:border-neutral-900 shadow-custom dark:shadow-custom-dark rounded-2xl flex flex-col justify-center items-center dark:bg-black p-8"
+      className="w-full h-full min-h-[400px] sm:min-h-[600px] font-light border dark:border-neutral-900 shadow-custom dark:shadow-custom-dark flex flex-col justify-center items-center dark:bg-black p-8"
     >
       <div className="justify-items-center mb-4 md:mb-16">
         <p className="text-lg md:text-xl lg:text-2xl text-teal-50">
