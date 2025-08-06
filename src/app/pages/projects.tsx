af81a6fe3bac6dd9fae81a84662a170015be23d8
Anthony Obiako <anthonyobiakoc@gmail.com>
2025-08-06 18:49:35 +0100

    added recent projects

--- src/app/pages/projects.tsx
+++ src/app/pages/projects.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import acme from "@/assets/acme-next.png";
+import spa from "@/assets/spa.png";
+import userDirectory from "@/assets/user-directory.png";
 import ExternalButton from "@/app/components/button";
 import portfolio from "@/assets/portfolio.png";
 import immureg from "@/assets/immureg-pic.png";
@@ -19,6 +21,44 @@ const Projects = () => {
         </h1>
       </div>
       <div className="mt-6 lg:mt-10">
+        <div className="px-3 py-5 sm:p-10 border-4 hover:border-solid border-dashed border-gray-800 rounded-md bg-gray-950 justify-items-center max-w-[900px] mb-8 md:mb-20 transform rotate-[-5deg] transition-transform duration-300 hover:rotate-0">
+          <h1 className="text-center text-xl text-slate-100 mb-3 sm:mb-5 md:text-2xl md:font-medium">
+            User Directory Web App
+          </h1>
+          <Image
+            src={userDirectory}
+            alt="user directory web app"
+            className="rounded-md mb-3"
+          />
+          <p className="mb-3 text-lg text-slate-100 md:text-xl">
+            A user directory web app that allows users to search for and view
+            profiles of individuals within an organization, enhancing internal
+            communication and collaboration.
+          </p>
+          <div className="w-full px-3 justify-around flex sm:max-w-[550px]">
+            <ExternalButton href="https://user-directory-project.vercel.app/">Live Preview</ExternalButton>
+            <ExternalButton href="https://github.com/Anthony-obiako/user-directory-project">Code</ExternalButton>
+          </div>
+        </div>
+        <div className="px-3 py-5 sm:p-10 border-4 hover:border-solid border-dashed border-gray-800 rounded-md bg-gray-950 justify-items-center max-w-[900px] mb-8 md:mb-20 transform rotate-[-5deg] transition-transform duration-300 hover:rotate-0">
+          <h1 className="text-center text-xl text-slate-100 mb-3 sm:mb-5 md:text-2xl md:font-medium">
+            Wellness Spa Booking App
+          </h1>
+          <Image
+            src={spa}
+            alt="acme dashboard"
+            className="rounded-md mb-3"
+          />
+          <p className="mb-3 text-lg text-slate-100 md:text-xl">
+            A wellness spa booking app that allows users to easily schedule and
+            manage appointments, enhancing customer experience and streamlining
+            spa operations.
+          </p>
+          <div className="w-full px-3 justify-around flex sm:max-w-[550px]">
+            <ExternalButton href="https://wellness-spa.vercel.app/">Live Preview</ExternalButton>
+            <ExternalButton href="https://github.com/Anthony-obiako/Wellness-Spa">Code</ExternalButton>
+          </div>
+        </div>
         <div className="px-3 py-5 sm:p-10 border-4 hover:border-solid border-dashed border-gray-800 rounded-md bg-gray-950 justify-items-center max-w-[900px] mb-8 md:mb-20 transform rotate-[-5deg] transition-transform duration-300 hover:rotate-0">
           <h1 className="text-center text-xl text-slate-100 mb-3 sm:mb-5 md:text-2xl md:font-medium">
             Health Care Service Modal
